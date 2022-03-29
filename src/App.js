c363c3a44cb638727d1c5b02784ab44808133bc0
= <rtyenriques@gmail.com>
2022-03-29 14:30:49 -0700

    installed packages and imported packages to index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/api/v1/categories/1', {
+  //     method: 'GET'
+  //   })
+  //   .then(response => response.json())
+  //   .then(data => console.log(data.articles))
+  // }
+
   render() {
     return (
       <div className="App">
-
+        "APP"
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
 
 import App from './App';
 
@@ -7,6 +11,8 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
